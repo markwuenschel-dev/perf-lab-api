3a6204022c7669ac0a62606912f5436720df1ffd
Nalakram <maw271190@gmail.com>
2026-04-21 18:00:32 -0400

    Update a002

--- alembic/versions/a002_planned_session_benchmark_cols.py
+++ alembic/versions/a002_planned_session_benchmark_cols.py
@@ -1,6 +1,6 @@
 """Add benchmark columns to planned_sessions.
 
-Revision ID: a002_planned_session_benchmark_cols
+Revision ID: a002_planned_bench_cols
 Revises: a001_benchmark_kpi
 Create Date: 2026-04-21
 """
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 
 
-revision: str = "a002_planned_session_benchmark_cols"
+revision: str = "a002_planned_bench_cols"
 down_revision: Union[str, None] = "a001_benchmark_kpi"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -31,4 +31,3 @@ def upgrade() -> None:
 def downgrade() -> None:
     op.execute("ALTER TABLE planned_sessions DROP COLUMN IF EXISTS benchmark_key")
     op.execute("ALTER TABLE planned_sessions DROP COLUMN IF EXISTS is_benchmark")
-
